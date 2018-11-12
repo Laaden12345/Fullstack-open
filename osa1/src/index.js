58bf8039842d1595276f3b67c6fef6d23e86ac71
Laaden12345 <erkkipentti123@gmail.com>
2018-11-12 17:35:32 +0200

    tehty 1.4 ja 1.5

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -3,47 +3,54 @@ import ReactDOM from 'react-dom'
 
 const Otsikko = (props) => {
   return (
-    <h1>{props.kurssi}</h1>
-  )
-}
-const Osa = (props) => {
-  return (
-    <p>{props.osa} {props.tehtavia}</p>
+    <h1>{props.kurssi['nimi']}</h1>
   )
 }
 const Sisalto = (props) => {
   return (
     <div>
-      <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
-      <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
-      <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
+      <Osa osa={props.osat[0]} />
+      <Osa osa={props.osat[1]} />
+      <Osa osa={props.osat[2]} />
     </div>
   )
 }
+const Osa = (props) => {
+  return (
+    <p>{props.osa['nimi']} {props.osa['tehtavia']}</p>
+  )
+}
+
 const Yhteensa = (props) => {
   return(
-    <p>yhteensä {props.yhteensa} tehtävää</p>
+    <p>yhteensä {props.osat[0]['tehtavia'] + props.osat[1]['tehtavia'] + props.osat[2]['tehtavia']} tehtävää</p>
   )
 }
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Otsikko kurssi={kurssi} />_
-      <Sisalto
-      osa1={osa1} tehtavia1={tehtavia1}
-      osa2={osa2} tehtavia2={tehtavia2}
-      osa3={osa3} tehtavia3={tehtavia3}
-      />
-      <Yhteensa yhteensa={tehtavia1 + tehtavia2 + tehtavia3} />
+      <Sisalto osat={kurssi.osat} />
+      <Yhteensa osat={kurssi.osat} />
     </div>
   )
 }
